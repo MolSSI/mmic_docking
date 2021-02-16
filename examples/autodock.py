46d094dd421424c55beede44382523bfa4f4d61d
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-02-16 12:55:53 -0500

    synced with latest code refactor

--- examples/autodock.py
+++ examples/autodock.py
@@ -1,15 +1,13 @@
-from mmelemental.models.molecule.mm_mol import Mol
+from mmelemental.models.molecule import Molecule
 from mmelemental.models.app.docking import DockInput
 
 # Construct docking input
-receptor = Mol.from_file("mmic_docking/data/PHIPA_C2/PHIPA_C2_apo.pdb")
-ligand = Mol.from_data(
-    "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O"
-)  # smiles code for ibuprofen
+receptor = Molecule.from_file("mmic_docking/data/PHIPA_C2/PHIPA_C2_apo.pdb")
+ligand = Molecule.from_data("CC(C)CC1=CC=C(C=C1)C(C)C(=O)O")  # smiles code for ibuprofen
 dockInput = DockInput(ligand=ligand, receptor=receptor)
 
 # Import simulation component for autodock vina
-from mmic_docking.components.autodock.autodock_component import AutoDockComponent
+from mmic_autodock.components.autodock_component import AutoDockComponent
 
 # Run autodock vina
 dockOutput = AutoDockComponent.compute(dockInput)
