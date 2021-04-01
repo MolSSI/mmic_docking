8d6af987ecaf133ffa66d173fb5788da0f9738bd
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-01 02:04:10 -0400

    update schema models + fields

--- mmic_docking/models/output.py
+++ mmic_docking/models/output.py
@@ -1,39 +1,24 @@
-from mmelemental.models.app.base import SimOutput
-from mmelemental.models.base import ProtoModel
-from mmelemental.models.molecule import Molecule
+from mmelemental.models.proc import ProcOutput
 from .common import DockMol
 from .input import DockInput
 from pydantic import Field, root_validator
 from typing import List, Optional
 
+__all__ = ["DockOutput"]
 
-class DockObserv(ProtoModel):
-    score: List[float] = Field(
-        ...,
-        description="Score for each pose. Typically the score is the binding affinity. Length must be equal to the number of poses.",
-    )
-
-
-class DockObservUnits(ProtoModel):
-    score: Optional[str] = Field(None, description="Score function unit.")
-
-
-class DockOutput(SimOutput):
-    simInput: DockInput = Field(..., description="Docking input model.")
+class DockOutput(ProcOutput):
+    proc_input: DockInput = Field(..., description="Docking input model.")
     poses: DockMol = Field(
         ...,
         description="A collection of ligand and optionally receptor poses. See the :class:``Ensemble`` class. ",
     )
-    observables: Optional[DockObserv] = Field(
-        None,
-        description="Physical observables for each pose. E.g. observables={'RMSD':[...]}.",
+    scores: List[float] = Field(
+        ...,
+        description="A list of scores for each pose. Typically the score is the binding affinity. Length must be equal to the number of poses.",
     )
-    observables_units: Optional[DockObservUnits] = Field(
-        None,
-        description="Physical observables units. E.g. observables_units={'RMSD':'angstrom'}.",
-    )
+    scores_units: Optional[str] = Field(None, description="Score function unit.")
 
     @root_validator
     def _valid_score_len(cls, values):
-        assert len(values.get("observables").score) == len(values["poses"].ligand)
+        assert len(values["scores"]) == len(values["poses"].ligand)
         return values
